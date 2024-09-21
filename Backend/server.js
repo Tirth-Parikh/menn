e16e0e20eea46de6366d52540cb845f1319abaa7
Tirth-Parikh <tirthparikh2203@gmail.com>
2024-09-21 14:31:44 +0530

    MENN

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); // Import cors
+
+const port = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+  });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
