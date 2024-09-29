d35b1c699fd4350af01ae4c329821fbbf91d92c8
Tirth-Parikh <tirthparikh2203@gmail.com>
2024-09-29 15:59:26 +0530

    tt

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path'); // Corrected path require statement
-const { OAuth2Client } = require('google-auth-library');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,37 +9,6 @@ app.use(express.json());
 
 // CORS middleware, allowing requests from the frontend running at localhost:3000
 app.use(cors({ origin: 'http://localhost:3000' }));
-
-// Example API route
-
-const client = new OAuth2Client(
-  '1077512729862-c5rvioghn5gretuli7upl9j7dk1ch6no.apps.googleusercontent.com', 
-  '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', 
-  'http://localhost:3000/' 
-);
-app.post('/api/auth/google', async (req, res) => {
-  const { code } = req.body;
-  console.log('tttt....>>>tttttt');
-  
-
-  try {
-      // Exchange the authorization code for tokens
-      const { tokens } = await client.getToken(code);
-      console.log('Tokens received:', tokens);
-
-      // Send the tokens back to the client, or save them to the session/database as needed
-      res.json(tokens);
-  } catch (error) {
-      console.error('Error exchanging code:', error);
-      res.status(500).json({ error: 'Failed to exchange authorization code' });
-  }
-});
-
-// app.get('/api/message', (req, res) => {
-//   res.json({ message: 'Hello, world!' });
-// });
-
-// Check if in production mode
 const chk = 'production'; 
 
 if (chk == 'production') {
